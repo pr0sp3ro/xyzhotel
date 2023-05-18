9c285c9a6d8ef01b124811062757b2c2c265e2b5
prospero <g0lden_vo1d@protonmail.com>
2023-05-18 20:00:42 +0200

    update

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
+    public function bookingsAll() {
+        return view('bookings.index');
+    }
+
+    public function bookingsEdit() {
+        return view('bookings.edit');
+    }
+
+    public function bookingsAdd() {
+        return view('bookings.add');
+    }
 }
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,18 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
     public function index() {
-        $user = Auth::user();
-
-        // $userId = $user->id;
-        $userEmail = $user->email;
-
-        return view('index', [
-            "userEmail" => $userEmail
-        ]);
+        return view('index');
     }
 }
