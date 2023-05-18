4a085f6f746fe52d496a5813452eae8255313318
prospero <g0lden_vo1d@protonmail.com>
2023-05-18 09:14:45 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [IndexController::class, "index"])->name('index');
+Route::get('/login', [AuthController::class, "login"])->name('login');
