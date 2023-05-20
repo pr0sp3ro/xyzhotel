1bee9ef7192d9e87ad2853d05261b62fa2efd2f8
prospero <g0lden_vo1d@protonmail.com>
2023-05-20 19:48:16 +0200

    update

--- src/app/Http/Controllers/BookingController.php
+++ src/app/Http/Controllers/BookingController.php
@@ -2,19 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    public function bookingsAll() {
-        return view('bookings.index');
+    public function index()
+    {
+        $bookings = Booking::all();
+        return response()->json($bookings);
     }
 
-    public function bookingsEdit() {
-        return view('bookings.edit');
+    public function create()
+    {
+        return response()->json([
+            'message' => 'Provide booking creation form or relevant information',
+        ]);
     }
 
-    public function bookingsAdd() {
-        return view('bookings.add');
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'room_id' => 'required|exists:rooms,id',
+            'place' => 'required|string',
+            'checkin_date' => 'required|date',
+            'checkout_date' => 'required|date',
+            'booking_date' => 'required|date',
+            'total_guests' => 'required|integer',
+            'num_adults' => 'required|integer',
+            'num_children' => 'required|integer',
+            'travel_purpose' => 'required|string',
+            'status' => 'required|string',
+        ]);
+
+        $booking = Booking::create($validatedData);
+
+        return response()->json([
+            'message' => 'Booking created successfully',
+            'booking' => $booking,
+        ]);
+    }
+
+    public function show(Booking $booking)
+    {
+        return response()->json($booking);
+    }
+
+    public function edit(Booking $booking)
+    {
+    }
+
+    public function update(Request $request, Booking $booking)
+    {
+    }
+
+    public function destroy(Booking $booking)
+    {
+        // Delete the specified booking
+        $booking->delete();
+
+        // Return a JSON response indicating success
+        return response()->json([
+            'message' => 'Booking deleted successfully',
+        ]);
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
+Route::get('/bookings/create', [BookingController::class, 'create'])->name('bookings.create');
+Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');
+Route::get('/bookings/{booking}', [BookingController::class, 'show'])->name('bookings.show');
+Route::get('/bookings/{booking}/edit', [BookingController::class, 'edit'])->name('bookings.edit');
+Route::put('/bookings/{booking}', [BookingController::class, 'update'])->name('bookings.update');
+Route::delete('/bookings/{booking}', [BookingController::class, 'destroy'])->name('bookings.destroy');
