d1b80d6c9d97384e449199e49f8fc29c83be305f
prospero <g0lden_vo1d@protonmail.com>
2023-05-20 21:00:15 +0200

    update

--- src/routes/api.php
+++ src/routes/api.php
@@ -8,16 +8,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth', 'gate:admin,employee'])->group(function () {
-    Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
-    Route::get('/bookings/{booking}', [BookingController::class, 'showOne']);
-    Route::get('/bookings/{booking}', [BookingController::class, 'isCleaned']);
+Route::middleware(['auth', 'gate:guest'])->group(function () {
+    // dodanie rezerwacji
+    Route::get('/bookings/create', [BookingController::class, 'createNewBooking']);
+    Route::post('/bookings', [BookingController::class, 'store']);
+
+    // usuniecie rezerwacji
+    Route::delete('/bookings/{booking}', [BookingController::class, 'deleteBooking']);
+
+    // wszystkie rezerwacje
+    Route::get('/bookings', [BookingController::class, 'index']);
+    // wyszukanie rezerwacji
+    Route::get('/bookings/{booking}', [BookingController::class, 'show']);
+
+    // edycja istniejacej rezerwacji
+    Route::get('/bookings/{booking}/edit', [BookingController::class, 'edit']);
+    Route::put('/bookings/{booking}', [BookingController::class, 'update']);
+
+    // sprawdzenie dostepnosci pokoi
+    // wyswietlenie historii rezerwacji
+    // potwierdzenie tozsamosci
 });
 
 Route::middleware(['auth', 'gate:admin'])->group(function () {
-    Route::post('/bookings', [BookingController::class, 'store']);
-    Route::get('/bookings/create-new-booking', [BookingController::class, 'createNewBooking']);
-    Route::get('/bookings/{booking}/edit', [BookingController::class, 'edit']);
-    Route::put('/bookings/{booking}', [BookingController::class, 'update']);
-    Route::delete('/bookings/{booking}', [BookingController::class, 'deleteBooking']);
 });
